d70fb4b6999aedaed805871ca92d9d40d22d5639
danhdeng <hui_danny_deng@hotmail.com>
2022-01-01 13:39:23 -0500

    commit the code before refactoring

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import validator from 'validator';
 
 function App() {
+  const [userSignUp, setUserSignUp] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const [errorMsg, setErrorMsg] = useState('');
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setUserSignUp({ ...userSignUp, [e.target.name]: e.target.value });
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (!validator.isEmail(userSignUp.email)) {
+      setErrorMsg('the email you input is invalid');
+    } else if (userSignUp.password.length < 5) {
+      setErrorMsg('the password length should be great than 5');
+    } else if (userSignUp.password !== userSignUp.confirmPassword) {
+      setErrorMsg('the passwords do not match');
+    }
+  };
   return (
     <div className="container my-5">
       <form>
@@ -14,6 +39,9 @@ function App() {
             id="email"
             type="email"
             className="form-control"
+            name="email"
+            value={userSignUp.email}
+            onChange={handleChange}
           />
         </div>
         <div className="mb-3">
@@ -24,7 +52,10 @@ function App() {
             placeholder="password"
             id="password"
             type="password"
+            name="password"
             className="form-control"
+            value={userSignUp.password}
+            onChange={handleChange}
           />
         </div>
         <div className="mb-3">
@@ -35,8 +66,21 @@ function App() {
             placeholder="confirm password"
             id="confirm-password"
             type="password"
+            name="confirmPassword"
             className="form-control"
+            value={userSignUp.confirmPassword}
+            onChange={handleChange}
           />
+        </div>
+        {errorMsg && <p>{errorMsg}</p>}
+        <div className="mb-3">
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={handleClick}
+          >
+            Submit
+          </button>
         </div>
       </form>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 // test('renders learn react link', () => {
@@ -7,9 +8,18 @@ import App from './App';
 //   expect(linkElement).toBeInTheDocument();
 // });
 
+beforeEach(() => {
+  console.log('before each test');
+  render(<App />);
+});
+
+afterEach(() => {
+  console.log('after each test');
+});
+
 test('textbox intial value should be empty', () => {
   // 1. rendering the component that we want to test
-  render(<App />);
+  // render(<App />);
 
   // 2.find the element that we want to test
   const emailInputElement = screen.getByRole('textbox');
@@ -21,4 +31,163 @@ test('textbox intial value should be empty', () => {
   expect(emailInputElement.value).toBe('');
   expect(passwordInputElement.value).toBe('');
   expect(confirmPasswordInputElement.value).toBe('');
+});
+
+test('user should be able to type an email in the textbox', () => {
+  // 1. rendering the component that we want to test
+  // render(<App />);
+
+  // 2. find the element that we want to test
+  const emailInputElement = screen.getByRole('textbox', { name: /email/i });
+
+  // 3. use the user event to simulate the test
+  userEvent.type(emailInputElement, 'dan@test.com');
+
+  // 4. assertion
+  expect(emailInputElement.value).toBe('dan@test.com');
+});
+
+test('user should be able to type an password', () => {
+  // 1. rendering the component that we want to test
+  // render(<App />);
+
+  // 2. find the element that we want to test
+  const passwordInputElement = screen.getByLabelText('Password');
+
+  // 3. use the user event to simulate the test
+  userEvent.type(passwordInputElement, 'password1234');
+
+  // 4. assertion
+  expect(passwordInputElement.value).toBe('password1234');
+});
+
+test('user should be able to type a confirm password', () => {
+  // 1. rendering the component that we want to test
+  // render(<App />);
+
+  // 2. find the element that we want to test
+  const confirmPasswordInputElement =
+    screen.getByLabelText(/confirm password/i);
+
+  // 3. use the user event to simulate the test
+  userEvent.type(confirmPasswordInputElement, 'password1234');
+
+  // 4. assertion
+  expect(confirmPasswordInputElement.value).toBe('password1234');
+});
+
+test('should show error message for invalid email', () => {
+  // 1. rendering the component that we want to test
+  // render(<App />);
+
+  let emailErrorElement = screen.queryByText(/the email you input is invalid/i);
+
+  expect(emailErrorElement).not.toBeInTheDocument();
+  // 2. find the element that we want to test
+  const emailInputElement = screen.getByRole('textbox', { name: /email/i });
+  const submitBtnElement = screen.getByRole('button', { name: /submit/i });
+
+  // 3. use the user event to simulate the test
+  userEvent.type(emailInputElement, 'dantest.com');
+
+  userEvent.click(submitBtnElement);
+
+  emailErrorElement = screen.queryByText(/the email you input is invalid/i);
+
+  // 4. assertion
+  expect(emailErrorElement).toBeInTheDocument();
+});
+
+test('the password length should be great than 5', () => {
+  // 1. rendering the component that we want to test
+  // render(<App />);
+
+  let passErrorElement = screen.queryByText(
+    /the password length should be great than 5/i
+  );
+
+  expect(passErrorElement).not.toBeInTheDocument();
+  // 2. find the element that we want to test
+  const emailInputElement = screen.getByRole('textbox', { name: /email/i });
+  const submitBtnElement = screen.getByRole('button', { name: /submit/i });
+  const passwordInputElement = screen.getByLabelText('Password');
+
+  // 3. use the user event to simulate the test
+  userEvent.type(passwordInputElement, '1234');
+  userEvent.type(emailInputElement, 'dantest@test.com');
+
+  userEvent.click(submitBtnElement);
+
+  passErrorElement = screen.queryByText(
+    /the password length should be great than 5/i
+  );
+
+  // 4. assertion
+  expect(passErrorElement).toBeInTheDocument();
+});
+
+test('the confirm password does not match', () => {
+  // 1. rendering the component that we want to test
+  // render(<App />);
+
+  let confirmPasswordErrorElement = screen.queryByText(
+    /the passwords do not match/i
+  );
+
+  expect(confirmPasswordErrorElement).not.toBeInTheDocument();
+  // 2. find the element that we want to test
+  const emailInputElement = screen.getByRole('textbox', { name: /email/i });
+  const submitBtnElement = screen.getByRole('button', { name: /submit/i });
+  const passwordInputElement = screen.getByLabelText('Password');
+  const confirmPasswordInputElement =
+    screen.getByLabelText(/confirm password/i);
+
+  // 3. use the user event to simulate the test
+  userEvent.type(confirmPasswordInputElement, 'password123');
+  userEvent.type(passwordInputElement, 'password1234');
+  userEvent.type(emailInputElement, 'dantest@test.com');
+
+  userEvent.click(submitBtnElement);
+
+  confirmPasswordErrorElement = screen.queryByText(
+    /the passwords do not match/i
+  );
+
+  // 4. assertion
+  expect(confirmPasswordErrorElement).toBeInTheDocument();
+});
+
+test('should pass all the tests', () => {
+  // 1. rendering the component that we want to test
+  // render(<App />);
+
+  // 2. find the element that we want to test
+  const emailInputElement = screen.getByRole('textbox', { name: /email/i });
+  const submitBtnElement = screen.getByRole('button', { name: /submit/i });
+  const passwordInputElement = screen.getByLabelText('Password');
+  const confirmPasswordInputElement =
+    screen.getByLabelText(/confirm password/i);
+
+  // 3. use the user event to simulate the test
+  userEvent.type(confirmPasswordInputElement, 'password1234');
+  userEvent.type(passwordInputElement, 'password1234');
+  userEvent.type(emailInputElement, 'dantest@test.com');
+
+  userEvent.click(submitBtnElement);
+
+  const emailErrorElement = screen.queryByText(
+    /the email you input is invalid/i
+  );
+  const passwordErrorElement = screen.queryByText(
+    /the password length should be great than 5/i
+  );
+
+  const confirmPasswordErrorElement = screen.queryByText(
+    /the passwords do not match/i
+  );
+
+  // 4. assertion
+  expect(emailErrorElement).not.toBeInTheDocument();
+  expect(passwordErrorElement).not.toBeInTheDocument();
+  expect(confirmPasswordErrorElement).not.toBeInTheDocument();
 });
